2e371d40cb1f148bcdc6028cd0da0e5869c05398
Waziziz <enzomouny2006@gmail.com>
2026-09-08 15:01:44 +0200

    Ex3 - fonction somme

--- Ex3/main.c
+++ Ex3/main.c
@@ -7,10 +7,23 @@
 * Environnement :
 *                - Cible / Microprocesseur : x86 64
 * 			     - IDE / Compilateur : Visual Studio 2026
-* Description : Programme qui demande à l'utilisateur de saisir un nombre entier et affiche sa valeur absolue.
+* Description : fonction somme 
 * ****************************************************************************************************************/
 
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+int somme(int a, int b) {
+    return a + b;
+}
+
 int main() {
+	int valeur1 = 12;
+	int valeur2 = 30;
+	int resultat = somme(valeur1, valeur2);
+
+	printf("la somme de %d et de %d est : %d\n", valeur1, valeur2, resultat);
+	
+	return 0;
+}
