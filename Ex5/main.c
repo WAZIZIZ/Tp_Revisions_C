790aeba597ea538cf853e7fc0125c3764e5b8be6
Waziziz <enzomouny2006@gmail.com>
2026-09-08 15:11:11 +0200

    Ex5 - Allocation dynamique

---
+++ Ex5/main.c
@@ -0,0 +1,42 @@
+/******************************************************************************************************************
+* Nom module fichier : main.c
+* auteur : MOUNY Enzo
+* Classe/Groupe : BTS CIEL2
+* Date : 08/09/2026
+* Projet : TP_Revision_c
+* Environnement :
+*                - Cible / Microprocesseur : x86 64
+* 			     - IDE / Compilateur : Visual Studio 2026
+* Description : Programme qui demande à l'utilisateur de saisir un nombre entier et affiche sa valeur absolue.
+* ****************************************************************************************************************/
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h> //nécessaire pour malloc et free
+
+int main() {
+	//declaration d'un pointeur sur caractere 
+	char* pointeurChar;
+
+	//utiliser le pointeur pour allouer dynamiquement 1 octet
+	pointeurChar = (char*)malloc(sizeof(char));
+
+	//verifier si l'allocation a reussi
+	if (pointeurChar == NULL) {
+		printf("Erreur d'allocation de memoire.\n");
+		return 1; // quitter le programme avec un code d'erreur
+	}
+
+	//affecter la lettre "l" a la memoire allouee 
+	*pointeurChar = 'l';
+
+	//afficher la valeur de la lettre stockee dans la memoire allouee
+	printf("La lettre stockee dans la memoire allouee est : %c\n", *pointeurChar);
+
+	//liberer la memoire allouee
+	free(pointeurChar);
+
+	return 0;
+
+}
